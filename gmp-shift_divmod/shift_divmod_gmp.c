98a6a9fd20e602c903528dea9a35e10565584085
Shlomi Fish <shlomif@shlomifish.org>
2020-09-14 10:57:08 +0300

    Extract a common module/library/header.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://refactoring.com/catalog/extractMethod.html
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- gmp-shift_divmod/shift_divmod_gmp.c
+++ gmp-shift_divmod/shift_divmod_gmp.c
@@ -26,47 +26,7 @@
 #include <stdio.h>
 
 #ifdef USE_SHIFT
-typedef struct
-{
-    mpz_t base, n, mask, shifted_input, lower_and_masked_input;
-    unsigned long shift;
-} shift_divmod_gmp__type;
-
-static void shift_divmod_gmp__init(
-    shift_divmod_gmp__type *const dest, mpz_t base, const unsigned long shift)
-{
-    dest->shift = shift;
-    mpz_init_set(dest->base, base);
-    mpz_init(dest->n);
-    mpz_mul_2exp(dest->n, dest->base, dest->shift);
-    mpz_init_set_ui(dest->mask, 1);
-    mpz_mul_2exp(dest->mask, dest->mask, dest->shift);
-    mpz_sub_ui(dest->mask, dest->mask, 1);
-    mpz_init(dest->shifted_input);
-    mpz_init(dest->lower_and_masked_input);
-}
-static void shift_divmod_gmp__clear(shift_divmod_gmp__type *const modder)
-{
-    mpz_clear(modder->base);
-    mpz_clear(modder->n);
-    mpz_clear(modder->mask);
-    mpz_clear(modder->shifted_input);
-    mpz_clear(modder->lower_and_masked_input);
-}
-
-static void shift_divmod_gmp__mod(
-    shift_divmod_gmp__type *const modder, mpz_t ret)
-{
-    if (mpz_cmp(ret, modder->n) >= 0)
-    {
-        mpz_div_2exp(modder->shifted_input, ret, modder->shift);
-        mpz_mod(modder->shifted_input, modder->shifted_input, modder->base);
-        mpz_mul_2exp(
-            modder->shifted_input, modder->shifted_input, modder->shift);
-        mpz_and(modder->lower_and_masked_input, ret, modder->mask);
-        mpz_ior(ret, modder->shifted_input, modder->lower_and_masked_input);
-    }
-}
+#include "shift_divmod_gmp.h"
 #endif
 
 #ifdef VERBOSE
