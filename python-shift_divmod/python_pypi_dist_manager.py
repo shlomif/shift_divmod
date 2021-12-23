881634a70a38b29a550c4c894a91931b914dbef9
Shlomi Fish <shlomif@shlomifish.org>
2021-12-23 13:04:49 +0200

    Extract a method or a function.
    This is Refactoring / code cleanup.
    
    See:
    
    * https://refactoring.com/catalog/extractMethod.html
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- python-shift_divmod/python_pypi_dist_manager.py
+++ python-shift_divmod/python_pypi_dist_manager.py
@@ -6,14 +6,7 @@
 #
 # Distributed under terms of the MIT license.
 
-import sys
-
 from pydistman import DistManager
-
-try:
-    cmd = sys.argv.pop(1)
-except IndexError:
-    cmd = 'build'
 
 dist_name = "shift_divmod"
 
@@ -31,4 +24,4 @@ obj = DistManager(
     full_name="Shlomi Fish",
     github_username="shlomif",
 )
-obj.run_command(cmd=cmd, args=[])
+obj.cli_run()
